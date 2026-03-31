7f8b94e01fc78c0ca460782c78400ee36cfa0fd6
Meet Sakhareliya <meetsakhareliya4702@gmail.com>
2026-03-31 21:36:23 +0530

    Saving payment everytime

--- src/main/java/com/distributed/reservation_system/service/PaymentService.java
+++ src/main/java/com/distributed/reservation_system/service/PaymentService.java
@@ -3,9 +3,10 @@ package com.distributed.reservation_system.service;
 import com.distributed.reservation_system.entity.Payment;
 import com.distributed.reservation_system.enums.PaymentStatus;
 import com.distributed.reservation_system.repository.PaymentRepository;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.Random;
@@ -15,6 +16,7 @@ import java.util.Random;
 public class PaymentService {
     private final PaymentRepository paymentRepository;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Payment pay(Integer totalAmount){
         PaymentStatus[] paymentStatuses = PaymentStatus.values();
         Payment payment = new Payment(BigDecimal.valueOf(totalAmount), "UPI", paymentStatuses[new Random().nextInt(paymentStatuses.length)]);
